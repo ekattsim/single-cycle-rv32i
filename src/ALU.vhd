84061ba3aa20629cc888aa8e570ac13200745431
Abhijeet Surakanti <aesbetic@gmail.com>
2025-08-05 09:01:20 -0400

    optimized ALU implementation to use a single CARRY4 primitive

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -7,7 +7,7 @@ entity ALU is
 		XLEN: integer
 	);
 	port (
-		a, b: in unsigned(XLEN-1 downto 0);
+		operand1, operand2: in unsigned(XLEN-1 downto 0);
 		opcode: in std_logic_vector(1 downto 0);
 		zero: out std_logic;
 		result: out unsigned(XLEN-1 downto 0)
@@ -15,22 +15,37 @@ entity ALU is
 end entity ALU;
 
 architecture ALU_ARCH of ALU is
+
+	signal logicResult: unsigned(XLEN-1 downto 0);
+	signal arithResult: unsigned(XLEN-1 downto 0);
 	signal result_s: unsigned(XLEN-1 downto 0);
+	signal opcode0: std_logic;
 
 begin
 
-	result <= result_s;
-	zero <= '1' when (result_s=0) else '0';
+	opcode0 <= opcode(0);
 
-	OPERATOR_SELECT: process (a, b, opcode)
+	LOGIC_UNIT: process(operand1, operand2, opcode0)
 	begin
-		case (opcode) is
-			when "00" => result_s <= a and b;
-			when "01" => result_s <= a or b;
-			when "10" => result_s <= a + b;
-			when "11" => result_s <= a - b;
-			when others => result_s <= (others => '0');
+		case (opcode0) is
+			when '0' => logicResult <= operand1 and operand2;
+			when others => logicResult <= operand1 or operand2;
 		end case;
 	end process;
+
+	ARITH_UNIT: process(operand1, operand2, opcode0)
+	begin
+		case (opcode0) is
+			when '0' => arithResult <= operand1 + operand2;
+			when others => arithResult <= operand1 - operand2;
+		end case;
+	end process;
+
+	ALU_CONTROL: with opcode(1) select
+		result_s <= logicResult when '0',
+					arithResult when others;
+
+	ZERO_CHECK: zero <= '1' when (result_s=0) else '0';
+	result <= result_s;
 
 end ALU_ARCH;
