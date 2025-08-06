fd098a16f126ce366519b7e35a1bad8783f3605f
Abhijeet Surakanti <aesbetic@gmail.com>
2025-08-06 17:18:35 -0400

    implemented RegFile with async read and sync write

--- src/RegFile.vhd
+++ src/RegFile.vhd
@@ -4,7 +4,7 @@ use ieee.numeric_std.all;
 
 entity RegFile is
 	port (
-		reset: std_logic;
+		reset, clock: std_logic;
 		rs1: in unsigned(4 downto 0);
 		rs2: in unsigned(4 downto 0);
 		rd: in unsigned(4 downto 0);
@@ -21,20 +21,22 @@ architecture RegFile_ARCH of RegFile is
 	type reg is array (0 to 31) of unsigned(31 downto 0);
 begin
 
-	RegisterFile: process (reset, rs1, rs2, rd, writeData, writeEn)
+	RegisterFile: process (reset, clock, rs1, rs2)
 		variable x: reg;
 	begin
-
-		-- x0 hardwired to 0
-		x(0) := (others => '0');
 
 		if (reset=ACTIVE) then
 			for i in 1 to 31 loop
 				x(i) := (others => '0');
 			end loop;
-		elsif (writeEn=ACTIVE) then
-			x(to_integer(rd)) := writeData;
+		elsif (rising_edge(clock)) then
+			if (writeEn=ACTIVE) then
+				x(to_integer(rd)) := writeData;
+			end if;
 		end if;
+
+		-- x0 hardwired to 0
+		x(0) := (others => '0');
 
 		rs1Data <= x(to_integer(rs1));
 		rs2Data <= x(to_integer(rs2));
