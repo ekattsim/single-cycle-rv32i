8db71b652a7d8b4d766493fc2439bfa785a446f4
Abhijeet Surakanti <aesbetic@gmail.com>
2025-08-13 10:26:59 -0400

    format/beautify using treesitter

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -3,49 +3,49 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity ALU is
-	generic (
-		XLEN: integer
-	);
-	port (
-		operand1, operand2: in std_logic_vector(XLEN-1 downto 0);
-		opcode: in std_logic_vector(1 downto 0);
-		zero: out std_logic;
-		result: out std_logic_vector(XLEN-1 downto 0)
-	);
+    generic (
+        XLEN : integer
+    );
+    port (
+        operand1, operand2 : in  std_logic_vector(XLEN-1 downto 0);
+        opcode             : in  std_logic_vector(1 downto 0);
+        zero               : out std_logic;
+        result             : out std_logic_vector(XLEN-1 downto 0)
+    );
 end entity ALU;
 
 architecture ALU_ARCH of ALU is
 
-	signal logicResult: std_logic_vector(XLEN-1 downto 0);
-	signal arithResult: std_logic_vector(XLEN-1 downto 0);
-	signal result_s: std_logic_vector(XLEN-1 downto 0);
-	signal opcode0: std_logic;
+    signal logicResult : std_logic_vector(XLEN-1 downto 0);
+    signal arithResult : std_logic_vector(XLEN-1 downto 0);
+    signal result_s    : std_logic_vector(XLEN-1 downto 0);
+    signal opcode0     : std_logic;
 
 begin
 
-	opcode0 <= opcode(0);
+    opcode0 <= opcode(0);
 
-	LOGIC_UNIT: process(operand1, operand2, opcode0)
-	begin
-		case (opcode0) is
-			when '0' => logicResult <= operand1 and operand2;
-			when others => logicResult <= operand1 or operand2;
-		end case;
-	end process;
+    LOGIC_UNIT : process(operand1, operand2, opcode0)
+    begin
+        case (opcode0) is
+            when '0'    => logicResult <= operand1 and operand2;
+            when others => logicResult <= operand1 or operand2;
+        end case;
+    end process;
 
-	ARITH_UNIT: process(operand1, operand2, opcode0)
-	begin
-		case (opcode0) is
-			when '0' => arithResult <= std_logic_vector(unsigned(operand1) + unsigned(operand2));
-			when others => arithResult <= std_logic_vector(unsigned(operand1) - unsigned(operand2));
-		end case;
-	end process;
+    ARITH_UNIT : process(operand1, operand2, opcode0)
+    begin
+        case (opcode0) is
+            when '0'    => arithResult <= std_logic_vector(unsigned(operand1) + unsigned(operand2));
+            when others => arithResult <= std_logic_vector(unsigned(operand1) - unsigned(operand2));
+        end case;
+    end process;
 
-	ALU_CONTROL: with opcode(1) select
-		result_s <= logicResult when '0',
-					arithResult when others;
+    ALU_CONTROL : with opcode(1) select
+        result_s <= logicResult when '0',
+                    arithResult when others;
 
-	ZERO_CHECK: zero <= '1' when (unsigned(result_s)=0) else '0';
-	result <= result_s;
+    ZERO_CHECK : zero <= '1' when (unsigned(result_s) = 0) else '0';
+    result            <= result_s;
 
 end ALU_ARCH;
--- src/RegFile.vhd
+++ src/RegFile.vhd
@@ -3,44 +3,44 @@ use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
 entity RegFile is
-	port (
-		reset, clock: std_logic;
-		rs1: in std_logic_vector(4 downto 0);
-		rs2: in std_logic_vector(4 downto 0);
-		rd: in std_logic_vector(4 downto 0);
-		writeData: in std_logic_vector(31 downto 0);
-		writeEn: in std_logic;
+    port (
+        reset, clock : in std_logic;
+        rs1          : in std_logic_vector(4 downto 0);
+        rs2          : in std_logic_vector(4 downto 0);
+        rd           : in std_logic_vector(4 downto 0);
+        writeData    : in std_logic_vector(31 downto 0);
+        writeEn      : in std_logic;
 
-		rs1Data: out std_logic_vector(31 downto 0);
-		rs2Data: out std_logic_vector(31 downto 0)
-	);
+        rs1Data : out std_logic_vector(31 downto 0);
+        rs2Data : out std_logic_vector(31 downto 0)
+    );
 end entity RegFile;
 
 architecture RegFile_ARCH of RegFile is
-	constant ACTIVE: std_logic := '1';
-	type reg is array (0 to 31) of std_logic_vector(31 downto 0);
+    constant ACTIVE : std_logic := '1';
+    type reg is array (0 to 31) of std_logic_vector(31 downto 0);
 begin
 
-	RegisterFile: process (reset, clock, rs1, rs2)
-		variable x: reg;
-	begin
+    RegisterFile : process (reset, clock, rs1, rs2)
+        variable x : reg;
+    begin
 
-		if (reset=ACTIVE) then
-			for i in 1 to 31 loop
-				x(i) := (others => '0');
-			end loop;
-		elsif (rising_edge(clock)) then
-			if (writeEn=ACTIVE) then
-				x(to_integer(unsigned(rd))) := writeData;
-			end if;
-		end if;
+        if (reset = ACTIVE) then
+            for i in 1 to 31 loop
+                x(i) := (others => '0');
+            end loop;
+        elsif (rising_edge(clock)) then
+            if (writeEn = ACTIVE) then
+                x(to_integer(unsigned(rd))) := writeData;
+            end if;
+        end if;
 
-		-- x0 hardwired to 0
-		x(0) := (others => '0');
+        -- x0 hardwired to 0
+        x(0) := (others => '0');
 
-		rs1Data <= x(to_integer(unsigned(rs1)));
-		rs2Data <= x(to_integer(unsigned(rs2)));
+        rs1Data <= x(to_integer(unsigned(rs1)));
+        rs2Data <= x(to_integer(unsigned(rs2)));
 
-	end process;
+    end process;
 
 end RegFile_ARCH;
